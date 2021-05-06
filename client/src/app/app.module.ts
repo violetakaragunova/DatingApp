73881bf289655ee768e1fbcdf98d075716e41690
violetakaragunova <37756789+violetakaragunova@users.noreply.github.com>
2021-05-06 15:22:54 +0200

    Section6 (#2)
    * Sec6-initial
    
    * End of Section 6
    
    Co-authored-by: Violeta Karagunova <violetak@iborn.net>

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,18 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { NavComponent } from './nav/nav.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { SharedModule } from './_modules/shared.module';
 
 
 @NgModule({
   declarations: [
+    AppComponent,
+    MemberListComponent,
+    MemberDetailComponent,
+    ListsComponent,
+    MessagesComponent,
     AppComponent,
     NavComponent,
     HomeComponent,
@@ -25,7 +35,7 @@ import { RegisterComponent } from './register/register.component';
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    BsDropdownModule.forRoot()
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
