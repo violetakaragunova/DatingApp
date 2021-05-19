85714c3724bf9b5d41a6be670832e34d809815ba
Violeta Karagunova <violetak@iborn.net>
2021-05-19 14:56:42 +0200

    End of section 16

---
+++ client/src/app/admin/photo-menagement/photo-menagement.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-menagement',
+  templateUrl: './photo-menagement.component.html',
+  styleUrls: ['./photo-menagement.component.css']
+})
+export class PhotoMenagementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
